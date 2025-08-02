65e1c261942ea6ee3e9ca4ab9c858f1f2d76e06c
Himanshu <221963184+Himanshuzer0@users.noreply.github.com>
2025-08-02 17:39:27 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) //we using nodejs , not browser
+
+
+console.log(3+3) // code redability should be high
+
+let  name = "Ram"
+let age = 25
+let isLoggedIn = false 
+
+console.table([ name, age, isLoggedIn ])
+
+// null is type of object 
+// find type of values 
+console.log(typeof "Ram");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
