c54b66602d6ebfc23f559d954703af04b92d289b
Himanshu <221963184+Himanshuzer0@users.noreply.github.com>
2025-08-02 17:39:41 +0000

    datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "ram@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed 
+
+ 
+
+accountEmail ="ram111@google.com"
+accountPassword = "54524"
+accountCity = "Mumbai"
+accountState = 
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,  accountPassword, accountCity, accountState])
