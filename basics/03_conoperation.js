ad194fe305d03af9fd3bf1b954c492b6f7e5179f
Himanshu <221963184+Himanshuzer0@users.noreply.github.com>
2025-08-03 16:16:54 +0000

    conversion done

--- basics/03_conoperation.js
+++ basics/03_conoperation.js
@@ -18,5 +18,36 @@ let booleanIsLockedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**********************Operations*****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "Golu"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("10" + 2);
+// console.log(1 +"2");
+// console.log("1" + 2 + 2); // this type of code generally not required 
+// console.log(1 + 2 + "2"); // this type of code generally not required 
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
