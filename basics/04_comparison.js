b382e1f321ce939517f1ee4e7e3351b9b6a3363a
Himanshu <221963184+Himanshuzer0@users.noreply.github.com>
2025-08-03 16:51:56 +0000

    comparison complete

---
+++ basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2<1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); // typescript does not allow to compare different data types
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// strict check ===
+
+console.log("2" === 2 );  // they strictely ckeck so not conversion because both are not same differ data type
