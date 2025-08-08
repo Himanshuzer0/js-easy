f6fb5569fbe60332ad1f9b1c4aaad031142d3938
Himanshu <221963184+Himanshuzer0@users.noreply.github.com>
2025-08-08 08:13:25 +0000

    maths done

---
+++ basics/06_maths.js
@@ -0,0 +1,32 @@
+const score = 400
+// console.log(score);
+ 
+
+const digit = new Number(100)
+// console.log(digit);
+// console.log(typeof digit);
+
+// console.log(digit.toString());
+
+// console.log(digit.toString().length);
+// console.log(digit.toFixed(1));
+
+const orNumber = 123.546
+// console.log(orNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(3,4,7,5,9,2));
+// console.log(Math.max(7,9,2,4,6,9,15));
+
+// console.log(Math.random());
+
+
+
