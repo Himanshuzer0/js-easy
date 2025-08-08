47129ff313fd4b154ceb5786329a93dd83b9c6c3
Himanshu <221963184+Himanshuzer0@users.noreply.github.com>
2025-08-08 09:06:08 +0000

    date done

---
+++ basics/07_dates.js
@@ -0,0 +1,31 @@
+let mDate = new Date()
+// console.log(mDate.toString());
+// console.log(mDate.toDateString());
+// console.log(mDate.toISOString());
+// console.log(mDate.toJSON());
+// console.log(mDate.toLocaleDateString());
+// console.log(mDate.toLocaleString());
+// console.log(mDate.toLocaleTimeString());
+// console.log(mDate.toTimeString());
+// console.log(mDate.toUTCString());
+// console.log(mDate.getTimezoneOffset());
+
+// console.log(typeof mDate);
+
+// let CreateDate = new Date(2025, 7,8)
+// console.log(CreateDate.toDateString());
+
+let CreateDate = new Date("2025-08-09")
+// console.log(CreateDate.toLocaleString());
+
+
+let nDate = new Date()
+console.log(nDate);
+console.log(nDate.getDate());
+console.log(nDate.getMonth() + 1);
+console.log(nDate.getDay());
+
+
+
+
+
