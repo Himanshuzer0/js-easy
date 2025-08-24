5ad79b7fdfe13018a138455a3ae20c62a313aec6
Himanshu <221963184+Himanshuzer0@users.noreply.github.com>
2025-08-24 19:16:13 +0000

    scope done

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -4,9 +4,44 @@ if(true){
 let  a = 10
 const b = 20
 var c = 30
-console.log( "Innervalue :",a )
+// console.log( "Innervalue :",a )
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "hanuman"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        //  console.log(website);
+        
+    }
+    // console.log(website);
+    two ()
+}
+// one ()
+
+if(true){
+    const username = "ram"
+    if(username === "ram"){
+        const website = " zomato"
+        // console.log(username + website)
+    }
+}
+
+//++++++++++++++++++++++ Interesting ++++++++++++++++++++++++++++
+console.log(addone(5))
+function addone(num){
+    return num + 1
+
+}
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+
+}
