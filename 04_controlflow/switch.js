325dc97ba1f9ae99f65378e29b6295a0f8ac1628
Himanshu <221963184+Himanshuzer0@users.noreply.github.com>
2025-08-27 16:36:24 +0000

    switch done

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,24 @@
+
+const month = "march"
+
+switch (month) {
+    case "jan" :
+        console.log("January");
+        break;
+
+    case "feb" : 
+    console.log("Febuary");
+    break ;
+
+    case "march" : 
+    console.log("March");
+    break ;
+
+    case "apr" : 
+    console.log("April");
+    break ;
+
+    default  :
+    console.log("default value print");
+    break;
+}
