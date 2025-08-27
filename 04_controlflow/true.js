5be40db6d9cf2477f84d8431ea836a8e6e1e2aca
Himanshu <221963184+Himanshuzer0@users.noreply.github.com>
2025-08-27 17:03:12 +0000

    true done

---
+++ 04_controlflow/true.js
@@ -0,0 +1,17 @@
+const userEmail = []
+
+if(userEmail) {
+    console.log("Got the userEmail");
+}
+else {
+    console.log("Don't have user Email");
+}
+
+// if(userEmail.length === 0){
+//     console.log("Array empty");
+// }
+
+const emptyObj = {}
+if(Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
