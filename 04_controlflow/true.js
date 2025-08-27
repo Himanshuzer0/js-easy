1785cde033b7462916b6552c8b78dc11ff609de9
Himanshu <221963184+Himanshuzer0@users.noreply.github.com>
2025-08-27 17:29:58 +0000

    true again done

--- 04_controlflow/true.js
+++ 04_controlflow/true.js
@@ -15,3 +15,16 @@ const emptyObj = {}
 if(Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
+
+
+// Nullish Coalescing Operator (??): null undefined 
+
+// let val1 ;
+// val1 = null ?? 10 ?? 25
+// console.log(val1)
+
+// Terniary operator
+// condition ? true : false
+
+const mPrice = 100
+mPrice <=80 ? console.log("less than 80")  : console.log("more than 80");
