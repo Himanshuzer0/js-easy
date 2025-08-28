e66b23e6ee6e44a6caf158b20cb3daae0c31527e
Himanshu <221963184+Himanshuzer0@users.noreply.github.com>
2025-08-28 19:04:22 +0000

    two done

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// let i = 0
+// while (i <= 10){
+//     console.log(`value of i is ${i}`);
+//     i = i + 2
+// }
+
+
+// let myArray = ['hero', 'bat', 'cat']
+//  let i = 0
+
+//  while(i < myArray.length){
+//     console.log(`value is ${myArray[i]}`);
+//     i++
+//  }
+
+
+// Do while loop
+    let i = 1
+
+    do{
+    console.log(`score is ${i}`);
+    i++
+    }
+    while(i <= 5)
