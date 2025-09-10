617b323cc6727b8747053acdefe3ba22376bcafd
Himanshu <221963184+Himanshuzer0@users.noreply.github.com>
2025-09-10 19:27:18 +0000

    oop done

---
+++ 13_classes_oops/oop.js
@@ -0,0 +1,27 @@
+// object literals 
+const user = {
+    username : "Himanshu",
+    logincount : 8,
+    signedin : true,
+
+// getUserDetails : function(){ 
+//     console.log(this);
+//    }
+}
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this)
+
+function User(username, logincount, isloggedin){
+    this.username = username;
+    this.logincount = logincount;
+    this.isloggedin = isloggedin;
+
+    return this
+
+}
+
+const userOne = new User("Hanuman", 15, true)
+const userTwo = new User("Himanshu", 25, false)
+console.log(userOne);
+console.log(userTwo);
